dd7dc174b31ab16bcf76f9966387e1d913aee345
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-03-02 15:12:06 +0530

    Added lf file manager configs and changed some picom and xmonad settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -52,7 +52,7 @@ myManageHook = composeAll . concat $
     ]
     where
     -- doShiftAndGo = doF . liftM2 (.) W.greedyView W.shift
-    myCFloats = ["Arandr", "Arcolinux-tweak-tool.py", "Arcolinux-welcome-app.py", "Galculator", "feh", "mpv", "Xfce4-terminal", "JetBrains Toolbox", "jetbrains-idea", "jetbrains-studio", "MEGAsync", "Variety"]
+    myCFloats = ["Arandr", "Arcolinux-tweak-tool.py", "Arcolinux-welcome-app.py", "Galculator", "feh", "mpv", "Xfce4-terminal", "JetBrains Toolbox", "MEGAsync", "Variety"]
     myTFloats = ["Downloads", "Save As..."]
     myRFloats = []
     myIgnores = ["desktop_window"]
