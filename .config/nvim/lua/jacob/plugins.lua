df4a24451dc02879d7ed5e77c925f3500c03f7de
Jacob Bosco <jacob.bosco3075@gmail.com>
2021-12-23 21:36:34 +0530

    Added basic completions to nvim

--- .config/nvim/lua/jacob/plugins.lua
+++ .config/nvim/lua/jacob/plugins.lua
@@ -33,16 +33,27 @@ return require("packer").startup(function(use)
 
   use "preservim/nerdtree"
 
-  -- TODO
-  use "ahmedkhalf/project.nvim"
-
   use "folke/which-key.nvim"
 
   use "blackCauldron7/surround.nvim"
+  --
+  -- TODO
+  use "ahmedkhalf/project.nvim"
 
   -- TODO
   use "p00f/nvim-ts-rainbow"
- 
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   use {
     "nvim-treesitter/nvim-treesitter", 
     run = ":TSUpdate" 
